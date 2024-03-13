d3a84c39893a68b310edea243f187f8962d08ae5
Jinwoo Lee <binaryWooo@gmail.com>
2024-03-13 11:37:46 +0900

    feat: network 모듈을 활용한 vpc 환경 구성

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "aws" {
   secret_key = var.secret_key
   region     = var.region
 }
+
+module "dev_network" {
+  source         = "./modules/network"
+  vpc_cidr_block = var.vpc_cidr_block
+  region_name    = var.region_name
+  env_name       = var.env_name_dev
+  terraform_name = var.terraform_name
+}
+
--- variables.tf
+++ variables.tf
@@ -12,3 +12,20 @@ variable "region" {
   description = "AWS region"
   default     = "ap-northeast-2"
 }
+
+variable "region_name" {
+  description = "AWS region name"
+  default     = "apne2"
+}
+
+variable "terraform_name" {
+  default = "pennyway"
+}
+
+variable "env_name_dev" {
+  default = "dev"
+}
+
+variable "vpc_cidr_block" {
+  default = "10.42.0.0/16"
+}
