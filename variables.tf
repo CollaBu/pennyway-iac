1c6570b93a621bff24378bf0121535b7c262ad20
Jinwoo Lee <binaryWooo@gmail.com>
2024-03-13 10:50:48 +0900

    feat: terraform provider 설정

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+  description = "AWS accesskey"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS accesskey"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "ap-northeast-2"
+}
