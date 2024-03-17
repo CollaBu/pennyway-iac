6b3521668b72eb6e7d37e3d06f2aac91032b095f
Jinwoo Lee <binaryWooo@gmail.com>
2024-03-17 18:14:34 +0900

    feat: 모듈을 활용하여 bastion, was 리소스 정의

--- main.tf
+++ main.tf
@@ -4,11 +4,34 @@ provider "aws" {
   region     = var.region
 }
 
-module "dev_network" {
+module "network" {
   source         = "./modules/network"
-  vpc_cidr_block = var.vpc_cidr_block
+  vpc_cidr_block = var.cidr_block
   region_name    = var.region_name
   env_name       = var.env_name_dev
   terraform_name = var.terraform_name
 }
 
+module "bastion" {
+  source         = "./modules/compute/bastion"
+  vpc_id         = module.network.vpc_id
+  vpc_cidr_block = var.cidr_block
+  subnet_id      = module.network.subnet_net_id
+  igw_id         = module.network.igw_id
+  region_name    = var.region_name
+  env_name       = var.env_name_dev
+  terraform_name = var.terraform_name
+  keypair        = var.keypair
+  remote_ip      = var.remote_ip
+}
+
+module "was" {
+  source         = "./modules/compute/was"
+  vpc_id         = module.network.vpc_id
+  vpc_cidr_block = var.cidr_block
+  subnet_id      = module.network.subnet_net_id
+  region_name    = var.region_name
+  env_name       = var.env_name_dev
+  terraform_name = var.terraform_name
+  keypair        = var.keypair
+}
