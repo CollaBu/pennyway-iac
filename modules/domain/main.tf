c29e7b95503d1bf704573f3b8c36c39520679a51
Jinwoo Lee <binaryWooo@gmail.com>
2024-04-01 09:34:48 +0900

    feat: 운영 환경 ns record로 dev 환경 추가

--- modules/domain/main.tf
+++ modules/domain/main.tf
@@ -1,9 +1,18 @@
-# 호스팅 영역 리소스 - production
+# 호스팅 영역 리소스 - 운영 환경
 resource "aws_route53_zone" "zone_prod" {
   name = var.domain
 }
 
 # 호스팅 영역 리소스 - 개발 환경 
-resource "aws_route53_zone" "name" {
+resource "aws_route53_zone" "zone_dev" {
   name = "dev.${var.domain}"
+}
+
+# 운영 환경 - 개발 환경 호스팅 영역 분리
+resource "aws_route53_record" "prod_to_dev" {
+  zone_id = aws_route53_zone.zone_prod.zone_id
+  name    = "dev.${var.domain}"
+  type    = "NS"
+  ttl     = "172800"
+  records = aws_route53_zone.zone_dev.name_servers
 }
