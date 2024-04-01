771fa78a3211ce81ec4827740a98611dbf2ba66c
Jinwoo Lee <binaryWooo@gmail.com>
2024-04-01 09:31:11 +0900

    feat: 개발 환경 hosting zone 선언

--- modules/domain/main.tf
+++ modules/domain/main.tf
@@ -1,4 +1,9 @@
-# 호스팅 영역 리소스
-resource "aws_route53_zone" "zone" {
+# 호스팅 영역 리소스 - production
+resource "aws_route53_zone" "zone_prod" {
   name = var.domain
+}
+
+# 호스팅 영역 리소스 - 개발 환경 
+resource "aws_route53_zone" "name" {
+  name = "dev.${var.domain}"
 }
