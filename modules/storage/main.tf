1b3780824aae4c088652e4fe18fd89f868eda3b2
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-02 02:24:07 +0900

    feat: s3 정책 설정

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,3 +1,48 @@
-resource "aws_s3_bucket" "storage" {
+# S3 버킷 리소스
+resource "aws_s3_bucket" "bucket" {
   bucket = "s3.${var.env_name}.${var.domain}"
+}
+
+# S3 버킷의 Public Access Block 설정
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# S3 버킷 접근 제어 정책 설정
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+# 다른 계정에서 S3 버킷에 접근할 수 있도록 권한 설정
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
+# S3 버킷 정적 웹사이트 호스팅 설정
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
 }
