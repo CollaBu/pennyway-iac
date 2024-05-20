ef990c426c9e5975680bbf9135794c421bef3d0d
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-20 10:18:08 +0900

    feat: cdn 리소스 선언

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -46,3 +46,47 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
     suffix = "index.html"
   }
 }
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for my S3 bucket"
+}
+
+resource "aws_cloudfront_distribution" "s3_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.bucket.arn
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "CDN for my S3 bucket"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.bucket.arn
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
