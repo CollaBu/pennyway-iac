09c02c5c15ba8604334cc7a3b3b3f4508a72f940
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-20 10:08:40 +0900

    fix: s3 접근 정책 수정 (비허용)

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -7,10 +7,10 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # S3 버킷 접근 제어 정책 설정
