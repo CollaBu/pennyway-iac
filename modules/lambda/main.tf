44f2a501ee4f754f09cdf2c59201546906539b5e
Jinwoo Lee <binaryWooo@gmail.com>
2024-06-02 21:31:33 +0900

    feat: lambda 함수 적용을 위한 변수 정의 및 image-resizer 첨부

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -27,3 +27,13 @@ resource "aws_iam_role_policy_attachment" "s3_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+# Lambda 함수 생성
+resource "aws_lambda_function" "lambda" {
+  function_name    = "lambda_s3"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  filename         = var.function_name
+  source_code_hash = filebase64sha256(var.function_name)
+}
---
+++ modules/lambda/variables.tf
@@ -0,0 +1,4 @@
+# lambda에서 실행할 함수 이름
+variable "function_name" {
+  type = string
+}
