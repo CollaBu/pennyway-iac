465d2d980f2ae1bdb5f89b1c5f1c05413c19dba9
Jinwoo Lee <binaryWooo@gmail.com>
2024-06-02 21:25:03 +0900

    feat: lambda 선언을 위한 iam 역할 정의

---
+++ modules/lambda/main.tf
@@ -0,0 +1,29 @@
+# IAM 역할 생성
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_s3_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM 역할 정책 첨부 - Lambda Basic Execution Role
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# IAM 역할 정책 첨부 - S3 Full Access
+resource "aws_iam_role_policy_attachment" "s3_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
