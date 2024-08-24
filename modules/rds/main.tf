c1596552695926e1e2089d4e9f59d1abb676858d
Jinwoo Lee <binaryWooo@gmail.com>
2024-08-24 20:56:46 +0900

    feat: rds 모듈 정의

---
+++ modules/rds/main.tf
@@ -0,0 +1,63 @@
+# rds security-group 정의
+resource "aws_security_group" "rds_sg" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-${var.env_name}-rds-sg"
+  }
+}
+
+# bastion 서버에서 net 서브넷에서 오는 접근 허용
+resource "aws_security_group_rule" "app_inbound_ssh" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = [var.app_cidr_block]
+  security_group_id = aws_security_group.rds_sg.id
+}
+
+# rds 서버에서 app 서브넷에서 오는 접근 허용
+resource "aws_security_group_rule" "data_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds_sg.id
+}
+
+# rds subnet group 정의
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "${var.terraform_name}-rds-subnet-group"
+  subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-${var.env_name}-rds-subnet-group"
+  }
+}
+
+
+# rds 리소스 정의
+resource "aws_db_instance" "rds" {
+  availability_zone     = "ap-northeast-2a"
+  identifier            = "${var.terraform_name}-${var.env_name}-rdb"
+  engine                = "mysql"
+  engine_version        = "8.0"
+  instance_class        = "db.t3.micro"
+  allocated_storage     = 20
+  max_allocated_storage = 100
+  storage_type          = "gp2"
+  username              = var.db_username
+  password              = var.db_password
+  parameter_group_name  = "default.mysql8.0"
+  skip_final_snapshot   = true
+  publicly_accessible   = true
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-${var.env_name}-rds"
+  }
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,45 @@
+# vpc
+variable "vpc_id" {
+  type = string
+}
+
+# 리소스명에서 참조되는 region_name
+variable "region_name" {
+  type = string
+}
+
+# 리소스명에서 참조되는 terraform_name(서비스명)
+variable "terraform_name" {
+  type = string
+}
+
+# 리소스명에서 참조되는 환경명
+variable "env_name" {
+  type = string
+}
+
+# DB에 접근하기 위한 username
+variable "db_username" {
+  type = string
+}
+
+# DB에 접근하기 위한 password
+variable "db_password" {
+  type = string
+}
+
+# rdb에 접근 가능한 ip
+variable "app_cidr_block" {
+  type = string
+}
+
+# rds 서브넷 그룹을 위한 subnet id 1
+variable "subnet_id_1" {
+  description = "ID of the first subnet for RDS"
+  type        = string
+}
+# rds 서브넷 그룹을 위한 subnet id 2
+variable "subnet_id_2" {
+  description = "ID of the second subnet for RDS"
+  type        = string
+}
